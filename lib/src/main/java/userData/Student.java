65235ec4748b080db769e1103f1bb12ec6866ca1
EleanorWilson <25942944@stu.mmu.ac.uk>
2025-11-17 15:00:12 +0000

    Further updates to student, id generator, email generator, dob generator

--- lib/src/main/java/userData/Student.java
+++ lib/src/main/java/userData/Student.java
@@ -2,6 +2,7 @@ package userData;
 import java.time.LocalDate;
 import java.util.Calendar;
 
+
 /**@Student
  * {@summary} This class is used to generate student objects and parameters for the student class. It contains getters and setters for student objects.
  */
@@ -14,8 +15,11 @@ public class Student {
 	private int yearOfStudy;
 	private Calendar dob;
 	private String password;
+	private String memorableWord;
 	
+	//--------------------------------------
 	// constructors
+	//--------------------------------------
 	
 	/**@Student
 	 * This is an empty constructor for student objects.
@@ -29,18 +33,53 @@ public class Student {
 	 * @param first - This is the first name of the student, must be a string.
 	 * @param last - This is the last name of the student, must be a string.
 	 * @param email - This is the email address of the student in the form first.last@stu.mmu.ac.uk. Can be generated with the generateEmail method of this class.
-	 * @param
+	 * @param subject - This is the subject the student is currently studying, must be a string.
+	 * @param yearOfStudy - This is the student's current year of study, must be an int.
+	 * @param dob - This is the student's date of birth (dob), must be of Calendar data type. This can be generated using Calendar constructor, with three integers for the day, month and year of birth.
+	 * @param password - This is the student's password for 'logging in' to their 'account'.
+	 * @param memorableWord - This is used to help the student if they have 'forgotten their password'
 	 */
-	public Student(int id, String first, String last, String email, String subject, int yearOfStudy, Calendar dob, String password) {
-		
+	public Student(int id, String first, String last, String email, String subject, int yearOfStudy, Calendar dob, String password, String memorableWord) {
+		this.id = id;
+		this.first = first;
+		this.last = last;
+		this.email = email;
+		this.subject = subject;
+		this.yearOfStudy = yearOfStudy;
+		this.dob = dob;
+		this.password = password;
+		this.memorableWord = memorableWord;
 	}
 	
 	
 	// generateId
+	public int generateId() {
+		// range = 250, for the maximum number of students that can be held
+		int id = (int)(Math.random()*250+1);
+		return id;
+	}
 	
 	// generateEmail
+	public String generateEmail(String first, String last) {
+		String email = first + last + "@stu.mmu.ac.uk";
+		return email;
+	}
 	
+	// generateDob
+	public Calendar generateDob(int year, int month, int day) {
+		Calendar dob = Calendar.getInstance();
+		dob.set(year, month, day);
+		return dob;
+	}
+	
+	//--------------------------------------
 	// getters
+	//--------------------------------------
+	
+	public int getId() {
+		return this.id;
+	}
+	
 	public String getFirst() {
 		return this.first;
 	}
@@ -69,7 +108,18 @@ public class Student {
 		return this.password;
 	}
 	
+	public String memorableWord() {
+		return this.memorableWord;
+	}
+	
+	//--------------------------------------
 	// setters
+	//--------------------------------------
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public void setFirst(String first) {
 		this.first = first;
 	}
@@ -96,6 +146,10 @@ public class Student {
 	
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public void setMemorableWord(String memorableWord) {
+		this.memorableWord = memorableWord;
 	}
 	
 }
