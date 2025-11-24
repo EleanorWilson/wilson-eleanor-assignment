4c34100725f19452208493817bc31d972864cdff
EleanorWilson <25942944@stu.mmu.ac.uk>
2025-11-24 11:20:49 +0000

    adding more testers for Student class

--- lib/src/test/java/userData/StudentTest.java
+++ lib/src/test/java/userData/StudentTest.java
@@ -55,4 +55,87 @@ class StudentTest {
 		
 	}
 	
+	@Test
+	@DisplayName("Set & Get First Name")
+	void testFirst() {
+		// set first no special characters # 1
+		test.setFirst("Eleanor");
+		assertEquals("Eleanor", test.getFirst());
+		
+		// set first no special characters # 2
+		test.setFirst("Robert");
+		assertEquals("Robert", test.getFirst());
+		
+		// set first no special characters # 3
+		test.setFirst("Gina");
+		assertEquals("Gina", test.getFirst());
+		
+		// set first special characters - @ symbol
+		test.setFirst("3Le@noR");
+		assertEquals("3Le@noR", test.getFirst());
+		
+		// set first special characters - \n newline symbol
+		test.setFirst("Elea\noR");
+		assertEquals("Elea\noR", test.getFirst());	
+		
+		// set first blank
+		test.setFirst("");
+		assertEquals("", test.getFirst());
+		
+	}
+	
+	@Test
+	@DisplayName("Set & Get Last Name")
+	void testLast() {
+		// set last no special characters # 1
+		test.setLast("Wilson");
+		assertEquals("Wilson", test.getLast());
+		
+		// set last no special characters # 2
+		test.setLast("Hritsaenko");
+		assertEquals("Hritsaenko", test.getLast());
+		
+		// set last no special characters # 3
+		test.setLast("Ross");
+		assertEquals("Ross", test.getLast());
+		
+		// set last special characters - * symbol
+		test.setLast("W1ls*n");
+		assertEquals("W1ls*n", test.getLast());
+		
+		// set last special characters - \n newline symbol
+		test.setLast("Wilso\n");
+		assertEquals("Wilso\n", test.getLast());
+		
+		// set last blank
+		test.setLast("");
+		assertEquals("", test.getLast());
+		
+	}
+	
+	@Test
+	@DisplayName("Set & Get Email")
+	void testEmail() {
+		// set email # 1
+		test.setEmail("eleanor.wilson@stu.mmu.ac.uk");
+		assertEquals("eleanor.wilson@stu.mmu.ac.uk", test.getEmail());
+		
+		// set email # 2
+		test.setEmail("robert.ross@stu.mmu.ac.uk");
+		assertEquals("robert.ross@stu.mmu.ac.uk", test.getEmail());
+		
+		// set email # 3
+		test.setEmail("spaces and no at symbol");
+		assertEquals("spaces and no at symbol", test.getEmail());
+		
+		// set email # 4
+		test.setEmail("8789534017504895");
+		assertEquals("8789534017504895", test.getEmail());
+		
+		// set email # 4
+		test.setEmail("---\n");
+		assertEquals("---\n", test.getEmail());
+		
+	}
+	
 }
